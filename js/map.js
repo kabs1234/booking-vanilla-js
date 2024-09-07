e2885a72c3dace371fd4635ba83794625ab98a92
Aikhan Zhashkeyev <spy.04@mail.ru>
2024-09-07 22:51:16 +0500

    implement all filters to housing types

--- js/map.js
+++ js/map.js
@@ -1,19 +1,21 @@
-const MAX_MARKERS = 10;
+import { MAX_MARKERS } from './constants.js';
+
 const mapCanvas = document.querySelector('.map__canvas');
 const mapFilters = document.querySelector('.map__filters');
-const housingType = mapFilters.querySelector('#housing-type');
-const housingPrice = mapFilters.querySelector('#housing-price');
-const housingRooms = mapFilters.querySelector('#housing-rooms');
-const housingGuests = mapFilters.querySelector('#housing-guests');
-const housingFeatures = mapFilters.querySelector('#housing-features');
+
 const addressInput = document.querySelector('#address');
 addressInput.value = '35.68832, 139.75438';
 
 
 const map = L.map(mapCanvas).setView([35.68832, 139.75438], 13);
-const marker = L.marker([35.688, 139.754], {draggable: true});
+const markerIcon = L.icon({
+  iconUrl: '../img/markers/marker-52.png',
+  iconSize: [52, 52],
+  iconAnchor: [26, 52],
+});
+const marker = L.marker([35.688, 139.754], {icon: markerIcon, draggable: true});
 marker.addTo(map);
-const userMarkersLayer = L.layerGroup();
+const usesrMarkersLayer = L.layerGroup();
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
   maxZoom: 19,
   attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
@@ -23,13 +25,77 @@ marker.addEventListener('move', () => {
   addressInput.value = `${lat.toFixed(5)}, ${lng.toFixed(5)}`;
 });
 
-const filterHousingType = (houseType, allOffers) => {
+const filterByHousingType = (houseType, allOffers) => {
   if (houseType === 'any') {
     return allOffers;
   }
 
   return allOffers.filter((ad) => ad.offer.type === houseType);
 };
+
+const filterByPrice = (price, allOffers) => {
+  if (price === 'any') {
+    return allOffers;
+  }
+
+  if (price === 'low') {
+    return allOffers.filter((ad) => ad.offer.price < 10000);
+  }
+
+  if (price === 'middle') {
+    return allOffers.filter((ad) => ad.offer.price >= 10000 && ad.offer.price < 50000);
+  }
+
+  if (price === 'high') {
+    return allOffers.filter((ad) => ad.offer.price >= 50000);
+  }
+};
+
+const filterByRooms = (rooms, allOffers) => {
+  if (rooms === 'any') {
+    return allOffers;
+  }
+
+  return allOffers.filter((ad) => Number(ad.offer.rooms) === Number(rooms));
+};
+
+const filterByGuests = (guests, allOffers) => {
+  if (guests === 'any') {
+    return allOffers;
+  }
+
+  return allOffers.filter((ad) => Number(ad.offer.guests) === Number(guests));
+};
+
+const filterByFeatures = (features, allOffers) => {
+  if (features.length === 0) {
+    return allOffers;
+  }
+
+  const includedFeaturesOffers = allOffers.filter((ad) => ad.offer.features);
+  const allIncludedFeaturesOffers = includedFeaturesOffers.filter((element) => features.every((value) => element.offer.features.includes(value)));
+
+  return allIncludedFeaturesOffers;
+};
+
+const applyAllFilters = (allOffers) => {
+  const housingType = mapFilters.querySelector('#housing-type');
+  const housingPrice = mapFilters.querySelector('#housing-price');
+  const housingRooms = mapFilters.querySelector('#housing-rooms');
+  const housingGuests = mapFilters.querySelector('#housing-guests');
+  const housingFeatures = [...mapFilters.querySelectorAll('.map__checkbox ')].filter((element) => element.checked).map((element) => element.value);
+
+  let allOffersCopy = [...allOffers];
+
+  allOffersCopy = filterByHousingType(housingType.value, allOffersCopy);
+  allOffersCopy = filterByPrice(housingPrice.value, allOffersCopy);
+  allOffersCopy = filterByRooms(housingRooms.value, allOffersCopy);
+  allOffersCopy = filterByGuests(housingGuests.value, allOffersCopy);
+  allOffersCopy = filterByFeatures(housingFeatures, allOffersCopy);
+
+  return allOffersCopy;
+};
+
 
 const makePopupFeatureList = (features) => {
   const list = features.map((feature) => `<li class="popup__feature popup__feature--${feature}"></li>`);
@@ -80,9 +146,14 @@ const makeOfferPopup = (offerData) => {
 const placeUserMarker = (offerLocation, offerData) => {
   const [lat, lng] = [offerLocation.lat, offerLocation.lng];
   const card = makeOfferPopup(offerData);
-  const userMarker = L.marker([lat, lng]).bindPopup(card).openPopup();
-  userMarker.addTo(userMarkersLayer);
-  userMarkersLayer.addTo(map);
+  const userMarkerIcon = L.icon({
+    iconUrl: '../img/markers/marker-40.png',
+    iconSize: [40, 40],
+    iconAnchor: [20, 40],
+  });
+  const userMarker = L.marker([lat, lng], {icon: userMarkerIcon}).bindPopup(card).openPopup();
+  userMarker.addTo(usesrMarkersLayer);
+  usesrMarkersLayer.addTo(map);
 };
 
 
@@ -97,26 +168,28 @@ const getOffers = async () => {
     errorElement.innerText = 'Похожие объявления не были загружены';
     document.body.append(errorElement);
     setTimeout(() => errorElement.remove(), 5000);
-    console.log(err);
   }
 };
 
 getOffers().then((offersInfo) => {
   const offersLocations = offersInfo.map((offer) => offer.location);
   const offersData = offersInfo.map((offer) => ({...offer.author, ...offer.offer}));
+
   for (let i = 0; i < MAX_MARKERS; i++) {
     placeUserMarker(offersLocations[i], offersData[i]);
   }
-  housingType.addEventListener('change', (evt) => {
-    const filteredByHousingOffers = filterHousingType(evt.target.value, offersInfo);
-    const filteredByHousingLocations = filteredByHousingOffers.map((offer) => offer.location);
-    const filteredByHousingData = filteredByHousingOffers.map((offer) => ({...offer.author, ...offer.offer}));
-    const filteredUserMarkersQuantity = filteredByHousingOffers.length >= 10 ? 10 : filteredByHousingOffers.length;
-    userMarkersLayer.clearLayers();
-    console.log(filteredByHousingOffers);
+
+  mapFilters.addEventListener('change', () => {
+    const filteredOffers = applyAllFilters(offersInfo);
+
+    const filteredOffersLocations = filteredOffers.map((offer) => offer.location);
+    const filteredOffersData = filteredOffers.map((offer) => ({...offer.author, ...offer.offer}));
+
+    const filteredUserMarkersQuantity = filteredOffers.length >= MAX_MARKERS ? MAX_MARKERS : filteredOffers.length;
+    usesrMarkersLayer.clearLayers();
 
     for (let i = 0; i < filteredUserMarkersQuantity; i++) {
-      placeUserMarker(filteredByHousingLocations[i], filteredByHousingData[i]);
+      placeUserMarker(filteredOffersLocations[i], filteredOffersData[i]);
     }
   });
 });
